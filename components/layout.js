fa4d2d14ab9a2511938d8383058b0ca38c23f982
Nghi1100 <vovannghi20@gmail.com>
2022-07-05 23:39:25 +0700

    the third commit

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Your Name";
-export const siteTitle = "Next.js Sample Website";
+const name = "Vo Van nghi";
+export const siteTitle = "Learn Next.JS";
 
 export default function Layout({ children, home }) {
   return (
